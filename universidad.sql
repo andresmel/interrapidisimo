128092b8fe7b4a2067aa615879fbc2b18b6d130a
andrew sosa <andrespeaker12@gmail.com>
2025-06-27 00:57:09 -0500

    se actualiza script de bases de datos

--- universidad.sql
+++ universidad.sql
@@ -14,6 +14,11 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
+
+-- Volcando estructura de base de datos para universidad
+CREATE DATABASE IF NOT EXISTS `universidad` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `universidad`;
+
 -- Volcando estructura para tabla universidad.clases
 CREATE TABLE IF NOT EXISTS `clases` (
   `id_clase` int NOT NULL AUTO_INCREMENT,
@@ -22,7 +27,19 @@ CREATE TABLE IF NOT EXISTS `clases` (
   PRIMARY KEY (`id_clase`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Volcando datos para la tabla universidad.clases: ~0 rows (aproximadamente)
+-- Volcando datos para la tabla universidad.clases: ~10 rows (aproximadamente)
+INSERT INTO `clases` (`id_clase`, `id_estudiante`, `id_materias_profesores`) VALUES
+	(1, 3, 1),
+	(2, 3, 3),
+	(3, 3, 5),
+	(4, 1, 2),
+	(5, 1, 10),
+	(6, 1, 4),
+	(7, 2, 6),
+	(8, 2, 1),
+	(9, 4, 1),
+	(10, 5, 5),
+	(11, 5, 7);
 
 -- Volcando estructura para tabla universidad.estudiantes
 CREATE TABLE IF NOT EXISTS `estudiantes` (
@@ -34,10 +51,13 @@ CREATE TABLE IF NOT EXISTS `estudiantes` (
   UNIQUE KEY `email` (`email`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Volcando datos para la tabla universidad.estudiantes: ~2 rows (aproximadamente)
+-- Volcando datos para la tabla universidad.estudiantes: ~5 rows (aproximadamente)
 INSERT INTO `estudiantes` (`id`, `nombre`, `email`, `password`) VALUES
 	(1, 'andres melo', 'andres@uniminuto.edu.co', '$2a$11$xZ2rXWyQjOiXWqA7RavKPO4QUlzzI7lDP6WUyocJRSIpT08u9XC86'),
-	(2, 'carlos medina', 'carlos@uniminuto.edu.co', '$2a$11$jZ4edK/SauHz4oWeO8jBKuUhJVbcbIOmeHEKnHT3pZG.Qv94l7Ey2');
+	(2, 'carlos medina', 'carlos@uniminuto.edu.co', '$2a$11$jZ4edK/SauHz4oWeO8jBKuUhJVbcbIOmeHEKnHT3pZG.Qv94l7Ey2'),
+	(3, 'maria escandon', 'maria@uniminuto.edu.co', '$2a$11$8GgjVFD9He0EoyorqEdOiOc5AIuQKWgxw10/2DZj/UQuXwN0Wv3gm'),
+	(4, 'laura mendez', 'laura@uniminuto.edu.co', '$2a$11$UbaUSMFWXhOC160LFjiRbukME4dkYUXJBbSUt20v8Mbriq1l79b3e'),
+	(5, 'raul martinez', 'raul@uniminuto.edu.co', '$2a$11$NULJU7Q8njcc8wBLn1TQUeQGkJz1HRoWfPVJimiUZmtUZcX7Efxuu');
 
 -- Volcando estructura para tabla universidad.materias
 CREATE TABLE IF NOT EXISTS `materias` (
